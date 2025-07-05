06f575ec14c44a0ab1130791ceb000c826ae9445
GaiaPiersanti <135056936+GaiaPiersanti@users.noreply.github.com>
2025-07-05 18:07:43 +0200

    stesso bug fix di prima

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -7,12 +7,12 @@ class Login extends Controller {
 
        if(isset($_POST['email'])) {
             // If the form is submitted, process the login
-            $user = $this->loadModel("User");
+            $user = $this->loadModel("user");
             $user->login($_POST);
 
         } elseif(isset($_POST['user_first_name']) && (isset($_POST['user_last_name']) && (!isset($_POST['email'])))) {
             // If the form is not submitted, just show the login page
-            $user = $this->loadModel("User");
+            $user = $this->loadModel("user");
             $user->register($_POST);
         }
 
--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -2,7 +2,7 @@
 class logout extends Controller {
     function index() {
         // This method will be called when the home page is accessed
-        $user = $this->loadModel("User");
+        $user = $this->loadModel("user");
         $user->logout();
     }
 }
